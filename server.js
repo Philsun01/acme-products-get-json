c72f33221adedf2d72a2ea22d72a2268a668b47f
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-02-06 20:07:44 -0800

    loaded product data via route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
     res.sendFile( path.join(__dirname, 'index.html') )
-})
+});
+
+app.get('/api/products', (req, res, next) => {
+    db.readJSON(path.join(__dirname, 'products.json'))
+    .then(data => res.send(data) )
+    
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
